8b6321aea1cf06bbf116e4e3e0fb015c6501e208
Ryan Coates <ryan.coates@gmail.com>
2018-09-26 18:11:48 -0600

    Add project files.

---
+++ Kaprekar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kaprekar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
